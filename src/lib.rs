ec73b2e4acc771d1ec96034efc3f744bca6e8127
Agustin Borrego <agustin@mixrank.com>
2024-06-16 11:05:43 +0200

    Made parse() more permissive with its input types

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,6 @@ mod tests;
 pub use data::{JsonElement, ParseError};
 
 /// Parses a JSON string into a [JsonElement], or returns a [ParseError].
-pub fn parse(json: &str) -> Result<JsonElement, ParseError> {
-    parser::JsonParser::from(json).parse()
+pub fn parse(json: impl AsRef<str>) -> Result<JsonElement, ParseError> {
+    parser::JsonParser::from(json.as_ref()).parse()
 }
